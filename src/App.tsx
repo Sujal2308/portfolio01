dae95f550a3cb121b201ceb415360af89f711bc8
Sebi <sebilune@duck.com>
2025-06-28 18:39:57 -0400

    feat: add basic header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { ThemeToggle } from "./components/ThemeToggle";
 
 function App() {
   return (
-    <div className="dark:bg-neutral-900 bg-neutral-200">
-      <div className="flex items-center justify-center w-screen h-screen">
-        <ThemeToggle />
+    <div className="min-h-screen dark:bg-neutral-800 bg-neutral-300 text-neutral-800 dark:text-neutral-300">
+      <div className="container max-w-2xl px-4 mx-auto pt-28">
+        <header className="flex items-center justify-between p-4">
+          <h1 className="text-lg font-medium">Sebi</h1>
+          <ThemeToggle />
+        </header>
+        {/* Content below header */}
       </div>
     </div>
   );
