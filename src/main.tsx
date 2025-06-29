ee8f8a4f379e9a9ca7a18902656810386469fda5
Sebi <sebilune@duck.com>
2025-06-29 14:42:14 -0400

    feat: add opaque dot pattern

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,16 @@ import "./styles/index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./providers/ThemeProvider.tsx";
 
+import { DotPattern } from "./components/magicui/dot-pattern.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <DotPattern
+        width={30}
+        height={30}
+        className="opacity-30 dark:opacity-10"
+      />
       <App />
     </ThemeProvider>
   </StrictMode>
