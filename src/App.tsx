d0bc4e594d494b73a759b857b46da39b464f5e3b
Sebi <sebilune@duck.com>
2025-06-29 14:08:55 -0400

    feat: add BlurFade on load using magicui

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import { Header } from "@/features/header";
 import { Intro } from "@/features/intro";
 import { Skills } from "@/features/skills";
 import { Projects } from "@/features/projects";
+import { BlurFade } from "@/components/magicui/blur-fade";
 
 function App() {
   return (
     <main className="text-zinc-700 dark:text-neutral-300">
       <article className="container max-w-3xl px-10 mx-auto mt-10 sm:mt-28">
         <div className="mb-12">
-          <Header className="mb-6" />
-          <Intro />
+          <BlurFade delay={0}>
+            <Header className="mb-6" />
+          </BlurFade>
+          <BlurFade delay={0.2}>
+            <Intro />
+          </BlurFade>
         </div>
         <div className="flex flex-col items-center w-full gap-8 mb-12 md:flex-row md:items-stretch">
-          <Skills />
-          <Projects />
+          <BlurFade delay={0.4}>
+            <Skills />
+          </BlurFade>
+          <BlurFade delay={0.6}>
+            <Projects />
+          </BlurFade>
         </div>
         <div className="mb-6">
           <h1 className="mb-6 text-xl font-semibold">Now</h1>
