12b6187ea5ac50e66e70e89dd94642218eff6268
Sebi <sebilune@duck.com>
2025-06-29 16:26:14 -0400

    feat: add avatar image

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -2,6 +2,8 @@ import { ThemeToggle } from "./components/ThemeToggle";
 
 import { Link } from "@/components/internal/Link";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 interface HeaderProps {
   className?: string;
 }
@@ -13,11 +15,17 @@ export function Header({ className }: HeaderProps) {
         "flex items-center justify-between" + (className ? " " + className : "")
       }
     >
-      <h1 className="text-xl font-semibold ">
-        <Link color="pink" link="https://github.com/sebilune">
-          Sebi
-        </Link>
-      </h1>
+      <div className="flex items-center gap-2">
+        <Avatar>
+          <AvatarImage src="https://github.com/sebilune.png" />
+          <AvatarFallback>S</AvatarFallback>
+        </Avatar>
+        <h1 className="text-xl font-semibold ">
+          <Link color="pink" link="https://github.com/sebilune">
+            Sebi
+          </Link>
+        </h1>
+      </div>
       <ThemeToggle />
     </header>
   );
