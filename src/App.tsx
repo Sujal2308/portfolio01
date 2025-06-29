7f2dedb4229d68e3dce4838cea325f570590ed1a
Sebi <sebilune@duck.com>
2025-06-29 14:59:30 -0400

    feat: swap blur direction from down to up

--- src/App.tsx
+++ src/App.tsx
@@ -12,34 +12,34 @@ function App() {
     <main className="text-zinc-700 dark:text-neutral-300">
       <article className="container max-w-3xl px-10 mx-auto mt-10 sm:mt-28">
         <div className="mb-12">
-          <BlurFade delay={0} blur="3px">
+          <BlurFade delay={0} direction="up" blur="3px">
             <Header className="mb-6" />
           </BlurFade>
-          <BlurFade delay={0.15} blur="3px">
+          <BlurFade delay={0.15} direction="up" blur="3px">
             <Intro />
           </BlurFade>
         </div>
         <div className="flex flex-col items-center w-full gap-8 mb-12 md:flex-row md:items-stretch">
-          <BlurFade delay={0.3} blur="3px">
+          <BlurFade delay={0.3} direction="up" blur="3px">
             <Skills />
           </BlurFade>
-          <BlurFade delay={0.45} blur="3px">
+          <BlurFade delay={0.45} direction="up" blur="3px">
             <Projects />
           </BlurFade>
         </div>
         <div className="mb-12">
-          <BlurFade delay={0.6} blur="3px">
+          <BlurFade delay={0.6} direction="up" blur="3px">
             <h1 className="mb-6 text-xl font-semibold">Now</h1>
           </BlurFade>
-          <BlurFade delay={0.75} blur="3px">
+          <BlurFade delay={0.75} direction="up" blur="3px">
             <Now />
           </BlurFade>
         </div>
         <div className="mb-12">
-          <BlurFade delay={0.9} blur="3px">
+          <BlurFade delay={0.9} direction="up" blur="3px">
             <h1 className="mb-6 text-xl font-semibold">Contact</h1>
           </BlurFade>
-          <BlurFade delay={1.05} blur="3px">
+          <BlurFade delay={1.05} direction="up" blur="3px">
             <Contact />
           </BlurFade>
         </div>
