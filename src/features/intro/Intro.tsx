553fdc13a79321879df1c09229ddd9e4fad93984
Sebi <sebilune@duck.com>
2025-06-28 21:08:38 -0400

    tune: tweak intro text

--- src/features/intro/Intro.tsx
+++ src/features/intro/Intro.tsx
@@ -6,9 +6,9 @@ export function Intro({ className }: IntroProps) {
   return (
     <p className={className}>
       <span className="font-bold font-newsreader">Bringing ideas to life.</span>
-      &nbsp;I am a full-stack Software Engineer based near Fort Lauderdale,
-      specialized in the development of modern websites, apps, tools, and
-      everything in between.
+      &nbsp;Full-stack Software Engineer based near Fort Lauderdale, specialized
+      in the development of modern websites, apps, tools, and everything in
+      between.
     </p>
   );
 }
