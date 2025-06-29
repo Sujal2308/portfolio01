10b560566429df09851b49bd1ad3d68b58a31a3e
Sebi <sebilune@duck.com>
2025-06-28 21:12:07 -0400

    style: decrease top header margin on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Intro } from "@/features/intro";
 function App() {
   return (
     <main className="text-zinc-700 dark:text-neutral-300">
-      <article className="container max-w-2xl px-4 mx-auto mt-28">
+      <article className="container max-w-2xl px-4 mx-auto mt-10 sm:mt-28">
         <div>
           <Header className="mb-6" />
           <Intro className="mb-6" />
