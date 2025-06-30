b2cab2aa1d6988a7b3a311f1059e295c3532c741
Sebi <sebilune@duck.com>
2025-06-29 22:26:27 -0400

    feat: add underline and icon to footer link

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,9 +1,17 @@
+import { ArrowUpRight } from "lucide-react";
+
 export function Footer() {
   return (
-    <footer className="py-4 text-sm border-t-2 border-natural-400 dark:border-neutral-700 text-neutral-400 dark:text-neutral-400 bg-background">
+    <footer className="py-4 text-sm border-t-2 border-natural-400 dark:border-neutral-700 text-neutral-400 bg-background">
       <div className="container flex justify-between max-w-3xl px-10 mx-auto">
-        <a href="https://youtu.be/gWI1d891COE?si=5kXxAofcSEvcBuUP&t=32">
-          <p>Engineers turn dreams into reality.</p>
+        <a
+          href="https://youtu.be/gWI1d891COE?si=5kXxAofcSEvcBuUP&t=32"
+          className="inline-flex items-center"
+        >
+          <p className="hover:underline hover:decoration-neutral-500 hover:decoration-2 hover:underline-offset-4">
+            Engineers turn dreams into reality.
+          </p>
+          <ArrowUpRight size={16} />
         </a>
         <p>2025</p>
       </div>
