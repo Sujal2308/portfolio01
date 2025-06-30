127731bb15e8119c67bc67cf3e4f8b46c2b9a737
Sebi <sebilune@duck.com>
2025-06-29 22:15:08 -0400

    style: normalize footer colors

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="py-4 text-sm border-t border-zinc-300 dark:border-neutral-700 text-neutral-500 dark:text-neutral-400 bg-background">
+    <footer className="py-4 text-sm border-t-2 border-natural-400 dark:border-neutral-700 text-neutral-400 dark:text-neutral-400 bg-background">
       <div className="container flex justify-between max-w-3xl px-10 mx-auto">
         <a href="https://youtu.be/gWI1d891COE?si=5kXxAofcSEvcBuUP&t=32">
           <p>Engineers turn dreams into reality.</p>
