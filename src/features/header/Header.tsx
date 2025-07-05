14d47194e3114a5dca33bb01c4c6233244dc144d
Sebi <sebilune@duck.com>
2025-07-05 15:18:00 -0400

    refactor(header): remove avatar from header

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,4 +1,3 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { BoxReveal } from "@/components/magicui/box-reveal";
 
 import { Link } from "@/components/internal/Link";
@@ -16,21 +15,6 @@ export function Header({ className }: HeaderProps) {
       }
     >
       <div className="flex items-center gap-2 group">
-        <div className="relative">
-          <a
-            href="https://github.com/sebilune"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Avatar className="w-14 h-14">
-              <AvatarImage
-                src="https://github.com/sebilune.png"
-                className="w-full h-full"
-              />
-              <AvatarFallback>S</AvatarFallback>
-            </Avatar>
-          </a>
-        </div>
         <h1 className="text-xl font-semibold ">
           <BoxReveal boxColor="#fb64b6" delay={0.025}>
             <Link color="pink" link="https://github.com/sebilune">
