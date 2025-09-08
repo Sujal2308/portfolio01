e52ff92a7eab70f4344ba3e0c8a65cc1c6e864ab
Sujal2308 <sujalbhugul08@gmail.com>
2025-09-09 00:02:58 +0530

    fix: remove outdated quote and update footer content for clarity

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -4,15 +4,14 @@ export function Footer() {
   return (
     <footer className="py-4 text-sm border-t-2 border-natural-400 dark:border-neutral-700 text-neutral-400 bg-background">
       <div className="container flex justify-between max-w-3xl px-10 mx-auto">
-        <a
-          href="https://youtu.be/gWI1d891COE?si=5kXxAofcSEvcBuUP&t=32"
-          className="inline-flex items-center group"
-        >
-          <p className="group-hover:underline group-hover:decoration-neutral-500 group-hover:decoration-2 group-hover:underline-offset-4">
-            Engineers turn dreams into reality.
-          </p>
-          <ArrowUpRight size={16} className="ml-0.5" />
-        </a>
+        <div className="inline-flex items-center gap-2">
+          <img
+            src="/svg/favicon/person.svg"
+            alt="person icon"
+            className="w-5 h-5"
+          />
+          <span>Thanks visiting my portfolio</span>
+        </div>
         <p>2025</p>
       </div>
     </footer>
